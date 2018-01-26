cdf4506d14e4556b823632ae6ced2a737f570693
unknown <mugidylan@gmail.com>
2018-01-26 18:37:42 +0400

    user account, authetication, routing section done

--- client/main.js
+++ client/main.js
@@ -1,26 +1,13 @@
 import { Meteor } from 'meteor/meteor';
-import React from 'react';
 import ReactDom from 'react-dom';
-import { Router, Route, browserHistory } from 'react-router';
-// import createBrowserHistory from 'history/createBrowserHistory';
+import { Tracker } from 'meteor/tracker';
 
-import Signup from '../imports/ui/Signup';
-import Login from '../imports/ui/Login';
-import Link from '../imports/ui/Link';
-import NotFound from '../imports/ui/NotFound';
+import {routes, onAuthChange} from '../imports/routes/routes';
 
-// const history = createBrowserHistory();
-
-const routes = (
-  <Router history={browserHistory}>
-    {/* <Switch> */}
-      <Route path="/" component={Login}/>
-      <Route path="/signup" component={Signup}/>
-      <Route path="/links" component={Link}/>
-      <Route path="*" component={NotFound}/>
-     {/* </Switch> */}
-  </Router>
-);
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
 
 Meteor.startup(() => {
   ReactDom.render(routes, document.getElementById('app'));
--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -6,33 +6,28 @@ import { Accounts } from 'meteor/accounts-base';
 export default class Signup extends React.Component {
   constructor(props){
     super(props);
-    // this.state = {
-    //   count: this.props.count || 0
-    // };
-
     this.state = {
       error: ''
     };
   }
 
-  // increment(){
-  //   this.setState({
-  //     count: this.state.count + 1
-  //   });
-  // }
-
   onSubmit(e) {
     e.preventDefault();
 
     let email = this.refs.email.value.trim();
-    let password = this.refs.password.trim();
-    Accounts.createUser({email, password}, (err) => {
-      console.log('Signup callback', err);
-    });
+    let password = this.refs.password.value.trim();
 
-    // this.setState({
-    //   error: 'something went wrong.'
-    // });
+    if(password.length < 9){
+      return this.setState({error: 'Password must be more than 8 characters long.'});
+    }
+
+    Accounts.createUser({email, password}, (err) => {
+      if(err){
+        this.setState({error: err.reason});
+      } else {
+        this.setState({error: ''});
+      }
+    });
   }
 
   render() {
@@ -42,7 +37,7 @@ export default class Signup extends React.Component {
 
         {this.state.error ? <p>{this.state.error}</p> : undefined}
 
-        <form onSubmit={this.onSubmit.bind(this)}>
+        <form onSubmit={this.onSubmit.bind(this)} noValidate>
           <input type='email' ref='email' name='email' placeholder='Email'/>
           <input type='password' ref='password' name='password' placeholder='password'/>
           <button>Create Account</button>
